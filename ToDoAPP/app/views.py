3c21ef6cad15439f6ab5b6518d4d20119a02a981
afzal khan <afzalkhan01765@gmail.com>
2025-04-27 20:10:20 +0600

    file add

--- ToDoAPP/app/views.py
+++ ToDoAPP/app/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect 
+from django.contrib import messages 
 
-# Create your views here.
+#import todo fom and model 
+
